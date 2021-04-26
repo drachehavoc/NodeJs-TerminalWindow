02bc5f0e0a8b2c714253eb0540e17c059522c26e
Daniel de Andrade Varela <daniel.andrade.varela@gmail.com>
2021-04-25 21:02:19 -0300

    some modiications are done to make it works as submodule

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "./d";
+export { TerminalWindow } from "./TerminalWindow"
+export { TerminalWindowGroup } from "./TerminalWindowGroup"
+export { TerminalWindowMenu, PanelMenu } from "./TerminalWindowMenu"
+export { Box, LinkedBox } from "./Box"
